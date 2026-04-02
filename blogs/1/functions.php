ab94d7809df7629da90528e2ada6232134f95d90
MarkusModulbuero <61046850+MarkusModulbuero@users.noreply.github.com>
2026-04-02 07:25:02 +0200

    Sportive Matomo

--- blogs/1/functions.php
+++ blogs/1/functions.php
@@ -10,6 +10,23 @@ function dsi_add_script_to_head() {
 <script> gtag('event', 'conversion', { 'send_to': 'AW-18005796338/JnDlCLPn9YUcEPLL6olD', 'value': 1.0, 'currency': 'EUR' }); 
     
 </script>
+
+<!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//statistik.antwortzeit.de/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '77']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
 <?php
 }
 add_action('wp_head', 'dsi_add_script_to_head');
+
